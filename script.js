9dd9c291b4e12f55927b16a6eebce3ead3396ca9
Ioakeim <joackim.gr@gmail.com>
2025-07-22 19:26:01 +0300

    Created the function for the first round

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     if (choice == 0) {
-        return "Rock";
+        return "rock";
     } else if (choice == 1) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 function getHumanChoice() {
     let humanChoice = prompt("Rock, Paper, Scissors?");
-    return humanChoice
+    return humanChoice.toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        return "You lose! Paper beats Rock";
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        return "You win! Rock beats Scissors";
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        return "You win! Paper beats Rock";
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        return "You lose! Scissors beat Paper";
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        return "You lose! Rock beats scissors";
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        return "You win! Scissors beat Paper";
+    } else if (humanChoice === computerChoice) {
+        return "Draw!";
+    }
+
+    
+}
+
+let humanScore = 0;
+let computerScore = 0;
+console.log(playRound(getHumanChoice(), getComputerChoice()));
