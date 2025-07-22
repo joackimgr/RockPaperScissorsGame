28304a6388001c362bf3abe74deeae1e50fe9c19
Ioakeim <joackim.gr@gmail.com>
2025-07-22 19:09:50 +0300

    Created the function for PC choice and Human choice

---
+++ script.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    if (choice == 0) {
+        return "Rock";
+    } else if (choice == 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+function getHumanChoice() {
+    let humanChoice = prompt("Rock, Paper, Scissors?");
+    return humanChoice
+}
